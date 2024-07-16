2123d412b74f2a5260e9f02b291c2e6470634a44
DavidRajlic <rajlic.david@gmail.com>
2024-07-17 00:42:11 +0200

    changed style in round js (matchday tables, editor secton)

--- frontend/src/pages/Rounds.js
+++ frontend/src/pages/Rounds.js
@@ -12,6 +12,7 @@ function Round() {
   const [matchday, setMatchday] = useState(1);
   const [groupedMatches, setGroupedMatches] = useState({});
   const [freeTeams, setFreeTeams] = useState({});
+  const [showEditor, setShowEditor] = useState(true);
 
   const navigate = useNavigate();
 
@@ -133,103 +134,162 @@ function Round() {
     });
   };
 
+  console.log(showEditor);
+
   return (
     <div>
-      <h1>Razpored</h1>
-
-      <h3>{matchday}. KOLO</h3>
-      <input
-        type="text"
-        placeholder="Datum"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-      />
-      <div>
-        <h4>Dodaj tekmo</h4>
-        <ul>
-          {teams.map((team) => (
-            <button
-              key={team._id}
-              type="button"
-              onClick={() => handleTeamClick(team)}
-              disabled={selectedTeams.length >= 2}
-            >
-              {team.name}
-            </button>
-          ))}
-        </ul>
+      <h1 className="matchdayTitle">Razpored</h1>
+      <div className={showEditor ? "showEditor" : "hideEditor"}>
+        <b>{matchday}. KOLO</b>
+        <input
+          className="dateInput"
+          type="text"
+          placeholder="Napiši datum kola"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
         <div>
-          <p>Izbrane ekipe:</p>
-          {selectedTeams.map((team, index) => (
-            <div key={index}>
-              <span>{team.name}</span>
-            </div>
-          ))}
-          {selectedTeams.length === 2 && (
-            <div>
-              <input
-                type="text"
-                placeholder="Čas tekme"
-                value={selectedTime}
-                onChange={(e) => setSelectedTime(e.target.value)}
-              />
-              <button onClick={addMatch}>Dodaj tekmo</button>
-            </div>
-          )}
-        </div>
-      </div>
-      {currentRoundMatches.length > 0 && (
-        <div>
-          <h4>Tekme za {matchday}. kolo:</h4>
-          {currentRoundMatches.map((match, index) => (
-            <div key={index}>
-              <span>
-                {match.team1} vs {match.team2}
-              </span>
-              <span> ob {match.time}</span>
-            </div>
-          ))}
-        </div>
-      )}
-      <button onClick={confirmMatchday}>Potrdi {matchday}. kolo</button>
-
-      <div>
-        <h2>Vsa Kola</h2>
-        {Object.keys(groupedMatches).map((day) => (
-          <div key={day}>
-            <h3>
-              {day}. KOLO {groupedMatches[day][0].date}
-            </h3>
-            {groupedMatches[day].map((match) => (
-              <div key={match._id}>
-                <span>
-                  {match.team1} vs {match.team2} ob {match.time}{" "}
-                  {match.matchPlayed && (
-                    <span style={{ fontWeight: "700", padding: "10px" }}>
-                      {" "}
-                      {match.team1Goals} : {match.team2Goals}{" "}
-                    </span>
-                  )}
-                  <button
-                    onClick={() =>
-                      editMatch(
-                        match.team1,
-                        match.team2,
-                        match._id,
-                        match.team1Id,
-                        match.team2Id
-                      )
-                    }
-                  >
-                    {" "}
-                    Uredi tekmo{" "}
-                  </button>
-                </span>
+          <ul className="selectTeamList">
+            {teams.map((team) => (
+              <button
+                key={team._id}
+                type="button"
+                onClick={() => handleTeamClick(team)}
+                disabled={selectedTeams.length >= 2}
+              >
+                <b> {team.name} </b>
+              </button>
+            ))}
+          </ul>
+          <div>
+            <p>Izbrane ekipe:</p>
+            {selectedTeams.map((team, index) => (
+              <div key={index}>
+                <span>{team.name}</span>
               </div>
             ))}
-            {freeTeams[day] && <p>Prosto: {freeTeams[day].join(", ")}</p>}
+            {selectedTeams.length === 2 && (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Čas tekme"
+                  value={selectedTime}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                />
+                <button onClick={addMatch}>Dodaj tekmo</button>
+              </div>
+            )}
           </div>
-        ))}
+        </div>
+        {currentRoundMatches.length > 0 && (
+          <div>
+            <h4>Tekme za {matchday}. kolo:</h4>
+            {currentRoundMatches.map((match, index) => (
+              <div key={index}>
+                <span>
+                  {match.team1} vs {match.team2}
+                </span>
+                <span> ob {match.time}</span>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <button className="confirmMatchday" onClick={confirmMatchday}>
+          Potrdi {matchday}. kolo
+        </button>
+      </div>
+
+      <div>
+        {showEditor ? (
+          <button
+            className="showEditorBtn"
+            onClick={() => setShowEditor(!showEditor)}
+          >
+            {" "}
+            Skrij orodje
+          </button>
+        ) : (
+          <button
+            className="hideEditorBtn"
+            onClick={() => setShowEditor(!showEditor)}
+          >
+            {" "}
+            Pokaži{" "}
+          </button>
+        )}
+      </div>
+      <div className="matchdayContainer">
+        {Object.keys(groupedMatches).map((day) => {
+          const matchPlayed = groupedMatches[day][0].matchPlayed; // Extracting the matchPlayed status from the first match of the day
+
+          return (
+            <div>
+              <h3>
+                {day}. KOLO {groupedMatches[day][0].date}
+              </h3>
+              <table className="matchdayTable" key={day}>
+                <tr>
+                  <th>TEKME</th>
+                  <th>{matchPlayed ? "REZULTAT" : "URA"}</th>
+                  <th> Uredi </th>
+                </tr>
+
+                {groupedMatches[day].map((match) => (
+                  <tr key={match._id}>
+                    <td>
+                      <span>
+                        {match.team1} : {match.team2}{" "}
+                      </span>
+                    </td>
+                    {match.matchPlayed ? (
+                      <td>
+                        <b>
+                          {" "}
+                          {match.team1Goals} : {match.team2Goals}{" "}
+                          {(match.team1Goals || match.team2Golas > 0) &&
+                            (match.team1Scorers.length === 0 ||
+                              match.team2Scorers === 0) && (
+                              <span> B.B</span>
+                            )}{" "}
+                        </b>
+                      </td>
+                    ) : (
+                      <td>{match.time}</td>
+                    )}
+                    <td>
+                      <button
+                        className="editMatchBtn"
+                        onClick={() =>
+                          editMatch(
+                            match.team1,
+                            match.team2,
+                            match._id,
+                            match.team1Id,
+                            match.team2Id
+                          )
+                        }
+                      >
+                        📝
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                {freeTeams[day] && (
+                  <tr className="freeTr">
+                    {" "}
+                    <td>
+                      <b>PROSTO </b>
+                      {freeTeams[day].join(", ")}{" "}
+                    </td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                )}
+              </table>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
