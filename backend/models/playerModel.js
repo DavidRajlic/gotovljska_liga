85343bc79fc9f4dbd7acf664f3a8757fb6540345
DavidRajlic <rajlic.david@gmail.com>
2024-08-03 17:50:00 +0200

    leader atribute to player model

--- backend/models/playerModel.js
+++ backend/models/playerModel.js
@@ -1,11 +1,14 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var playerSchema = new Schema({
-	'name' : String,
-	'goalsScored' : Number,
-	'yellowCards' : Number,
-	'redCards' : Number
+  name: String,
+  goalsScored: Number,
+  yellowCards: Number,
+  redCards: Number,
+  mustPayYellowCard: Boolean,
+  mustPayRedCard: Boolean,
+  leader: Boolean,
 });
 
-module.exports = mongoose.model('player', playerSchema);
+module.exports = mongoose.model("player", playerSchema);
