32752cc78f2be60c26b37a8cc78b6f614b517a24
DavidRajlic <rajlic.david@gmail.com>
2024-08-06 13:53:06 +0200

    leaderboard is responsive

--- frontend/src/pagesAdmin/Leaderboard.js
+++ frontend/src/pagesAdmin/Leaderboard.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 function Teams() {
   const [teams, setTeams] = useState([]);
-  const [matches, setMatches] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -20,7 +19,7 @@ function Teams() {
         const sortedTeams = sortTeams(teamsData, matchesData);
 
         setTeams(sortedTeams);
-        setMatches(matchesData);
+
         setIsLoading(false);
       } catch (error) {
         console.error("Prišlo je do napake pri pridobivanju podatkov!", error);
@@ -37,7 +36,6 @@ function Teams() {
         return b.points - a.points; // sorted by points
       } else {
         const headToHeadResult = getHeadToHeadResult(a, b, matches);
-        console.log(headToHeadResult, a.name, b.name);
         if (headToHeadResult !== 0) {
           return headToHeadResult;
         } else {
@@ -61,7 +59,7 @@ function Teams() {
 
     matches.forEach((match) => {
       if (
-        // checking if macthes where these two teams played each other
+        // checking matches where these two teams played each other
         (match.team1Id === teamA._id && match.team2Id === teamB._id) ||
         (match.team1Id === teamB._id && match.team2Id === teamA._id)
       ) {
@@ -109,61 +107,52 @@ function Teams() {
     );
   }
 
-  // if there are no teams display the message
-  if (teams.length === 0) {
-    return (
-      <div>
-        <h2 style={{ textAlign: "center", color: "red", marginTop: "5rem" }}>
-          Liga trenutno ne poteka!
-        </h2>
-      </div>
-    );
-  }
-
   return (
     <div className="leaderboard">
-      <h1>Lestvica</h1>
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Ekipa</th>
-              <th>Odigrane tekme</th>
-              <th>Zmage</th>
-              <th>Remi</th>
-              <th>Porazi</th>
-              <th>Točke</th>
-              <th>Doseženi zadetki</th>
-              <th>Prejeti zadetki</th>
-              <th>Gol razlika</th>
-              <th>Kartoni</th>
-            </tr>
-          </thead>
-          <tbody>
-            {teams.map((team, index) => (
-              <tr className="tr" key={team._id}>
-                <td>
-                  <b>{index + 1}.</b>
-                </td>
-                <td>
-                  <b>{team.name}</b>
-                </td>
-                <td>{team.matchesPlayed}</td>
-                <td>{team.wins}</td>
-                <td>{team.draws}</td>
-                <td>{team.losses}</td>
-                <td className="points">
-                  <b>{team.points}</b>
-                </td>
-                <td>{team.goalsScored}</td>
-                <td>{team.goalsConceded}</td>
-                <td>{team.goalDiffrence}</td>
-                <td>{team.yellowCards + team.redCards}</td>
+        <h2>Lestvica</h2>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th className="teamsTh">Ekipa</th>
+                <th className="matchesPlayedTh"></th>
+                <th className="winsTh"></th>
+                <th className="drawsTh"></th>
+                <th className="lossesTh"></th>
+                <th className="pointsTh"></th>
+                <th className="goalsScoredTh"></th>
+                <th className="goalsConcededTh"></th>
+                <th className="goalDiffrenceTh"></th>
+                <th className="cardsTh"></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {teams.map((team, index) => (
+                <tr className="tr" key={team._id}>
+                  <td>
+                    <b>{index + 1}.</b>
+                  </td>
+                  <td>
+                    <b>{team.name}</b>
+                  </td>
+                  <td>{team.matchesPlayed}</td>
+                  <td>{team.wins}</td>
+                  <td>{team.draws}</td>
+                  <td>{team.losses}</td>
+                  <td className="points">
+                    <b>{team.points}</b>
+                  </td>
+                  <td>{team.goalsScored}</td>
+                  <td>{team.goalsConceded}</td>
+                  <td>{team.goalDiffrence}</td>
+                  <td>{team.yellowCards + team.redCards}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
