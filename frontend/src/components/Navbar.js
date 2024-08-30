19e91473e81f631846dd0729bce866609718708b
DavidRajlic <rajlic.david@gmail.com>
2024-08-30 09:59:11 +0200

    updated link of rounds

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -39,7 +39,7 @@ function Navbar() {
 
           <Link to="/rounds" onClick={toggleMenu}>
             {" "}
-            <li> Razpored/Rezultati </li>
+            <li> Razpored & Rezultati </li>
           </Link>
 
           <Link to="/scorers" onClick={toggleMenu}>
